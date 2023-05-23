a41fe8a45e39e0fa9961a06b24b4d47ee8160716
devdave0 <dbchung24@gmail.com>
2023-05-23 10:13:29 -0700

    created app sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import styles from './styles'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-primary w-full overflow-hidden'>
+
+      {/* Navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      {/* Hero Section */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero Component
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Business
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+          Footer
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
